434130e3bd478b0a322e29e2bd95a5a82b9a40c2
mkubasz <mkubasz@gmail.com>
2019-08-09 01:09:04 +0200

    Some of working code

--- duck_main/src/lib.rs
+++ duck_main/src/lib.rs
@@ -3,10 +3,13 @@ use std::error::Error;
 use std::env;
 use std::collections::HashSet;
 use std::borrow::{Borrow, BorrowMut};
-use ndarray::Data;
+use ndarray::prelude::*;
+use std::ops::{Index, IndexMut};
+use rustlearn::array::dense::ArrayIterator;
 
 #[derive(Hash, Eq, PartialEq, Debug, Clone)]
 pub struct Element {
+    label: String,
     value: String
 }
 
@@ -27,6 +30,15 @@ impl Column {
         }
     }
 
+    pub fn values(&self) -> Array1<f32> {
+        let mut arr = Array::default(self.data.len());
+
+        for (index, element) in self.data.iter().enumerate() {
+            arr[index] = element.value.parse().unwrap();
+        }
+        arr
+    }
+
     fn push(&mut self, element: Element) {
         self.data.push(element);
     }
@@ -34,12 +46,10 @@ impl Column {
     pub fn unique(&mut self) -> Column {
         let mut unique_values = HashSet::new();
         self.data.iter().for_each(|e| {
-            unique_values.insert(Element {
-                value: e.value.clone()
-            });
+            unique_values.insert(e.clone());
         });
         let mut column = Column::new();
-        unique_values.iter().for_each(|el| column.data.push(Element { value: el.value.clone()}) );
+        unique_values.iter().for_each(|el| column.data.push(el.clone()) );
         column
     }
 
@@ -50,14 +60,27 @@ impl Column {
 
 #[derive(Debug, Clone)]
 pub struct DataFrame {
-    label: Vec<String>,
+    labels: Vec<String>,
     data: Vec<Column>
+}
+
+impl Index<usize> for DataFrame {
+    type Output = Column;
+    fn index(&self, i: usize) -> &Self::Output {
+        &self.data[i]
+    }
+}
+
+impl IndexMut<usize> for DataFrame {
+    fn index_mut(& mut self, i: usize) -> &mut Self::Output {
+        &mut self.data[i]
+    }
 }
 
 impl DataFrame {
     pub fn new(size: usize) -> DataFrame {
         DataFrame {
-            label: Vec::new(),
+            labels: Vec::new(),
             data: vec![Column::new(); size]
         }
     }
@@ -65,15 +88,21 @@ impl DataFrame {
     fn push(&mut self, element: Vec<Element>) {
         for (i, el) in element.iter().enumerate() {
             self.data[i].push(Element {
+                label: el.label.clone(),
                 value: el.value.clone()
             });
         }
     }
 
     pub fn by(&mut self, label: &str) -> &mut Column {
-        let index = self.label.clone().iter().position(|el| el == label ).unwrap();
+        let index = self.labels.clone().iter().position(|el| el == label ).unwrap();
         &mut self.data[index]
     }
+
+//    pub fn many(&mut self, labels: Vec<String>) -> &mut DataFrame {
+//        let indexes = self.labels.clone().iter().positions(|el| labels.contains(el) ).unwrap();
+//        &mut self.data[indexes]
+//    }
 
     pub fn get_dummies(&mut self, label: &str) -> DataFrame {
         let column = self.by(label);
@@ -81,38 +110,54 @@ impl DataFrame {
         let size = unique_column.clone().len();
         let columns = column.data.iter().map(|el| {
             let mut tmp = vec![0;size];
-            let index = unique_column.data.iter().position(|it| it == el).unwrap();
+            let index = unique_column.data.iter().position(|it| it.value == el.value).unwrap();
             tmp[index] = 1;
             tmp
         }).collect();
-        let mut df = Self::from_vec(columns, size);
+        let mut df = Self::from_vec(columns, size, unique_column.clone());
         df.add_labels(unique_column.data.iter().map(|el| el.value.clone()).collect());
         df
     }
     pub fn concat(&mut self, df: DataFrame) -> DataFrame {
-        DataFrame{ label: [&self.label[..], &df.label[..]].concat(), data: [&self.data[..], &df.data[..]].concat() }
+        DataFrame{ labels: [&self.labels[..], &df.labels[..]].concat(), data: [&self.data[..], &df.data[..]].concat() }
     }
 
     pub fn drop(&mut self, label: &str) -> DataFrame {
-        let position = self.label.clone().iter().position(|el| el == label ).unwrap();
-        self.label.remove(position);
+        let position = self.labels.clone().iter().position(|el| el == label ).unwrap();
+        self.labels.remove(position);
+        self.data.remove(position);
+        self.to_owned()
+    }
+    pub fn idx_drop(&mut self, position: usize) -> DataFrame {
+        self.labels.remove(position);
         self.data.remove(position);
         self.to_owned()
     }
 
-    pub fn from_vec(vec: Vec<Vec<i32>>, size: usize)-> DataFrame {
+    pub fn from_vec(vec: Vec<Vec<i32>>, size: usize, labels: Column)-> DataFrame {
         let mut df = DataFrame::new(size);
         for (_, columns) in vec.iter().enumerate() {
             for (index, value) in columns.iter().enumerate() {
-                df.data[index].push(Element {value: format!("{}", value)});
+                df.data[index].push(Element { label: labels.data[index].value.clone(), value: format!("{}", value)});
             }
         }
         df
     }
 
     pub fn add_labels(&mut self, labels: Vec<String>) -> &DataFrame {
-        self.label = labels.clone();
+        self.labels = labels.clone();
         self
+    }
+
+    pub fn values(&self) -> Vec<Vec<f32>> {
+        //let mut arr = Array::default((self.labels.len(), self.data.len()));
+        let mut arr = vec![vec![0.0;self.labels.len()];self.data[0].data.len()];
+        for (i, columns) in self.data.iter().enumerate() {
+            for (j, el) in columns.data.iter().enumerate() {
+                arr[j][i] = el.value.parse().unwrap();
+            }
+        }
+        arr
     }
 
     pub fn read_csv(file_name: String) -> Result<DataFrame, Box<dyn Error>> {
@@ -124,18 +169,17 @@ impl DataFrame {
         let mut df = DataFrame::new(rdr.headers().unwrap().len());
         for header in rdr.headers() {
             for el in header.iter() {
-                df.label.push(el.to_string());
+                df.labels.push(el.to_string());
             }
         }
         for result in rdr.records(){
             let mut row =Vec::new();
             let record = result?;
-            for el in record.iter() {
-                row.push(Element {value: el.to_string() });
+            for (index, el) in record.iter().enumerate() {
+                row.push(Element {label: df.labels[index].clone(), value: el.to_string() });
             }
             df.push(row);
         }
-        print!("{:?}", df);
         Ok(df)
     }
 }
--- duck_main/src/main.rs
+++ duck_main/src/main.rs
@@ -2,19 +2,30 @@ use rustlearn::prelude::*;
 use rustlearn::linear_models::sgdclassifier::Hyperparameters;
 use rustlearn::datasets::iris;
 use duck_main::{DataFrame, Element};
+use rustlearn::trees::decision_tree;
 
 fn main() {
     let mut df = DataFrame::read_csv(format!("src/Startups.csv")).unwrap();
-    let dummies = df.get_dummies("State");
-    let mut new_df = df.concat(dummies).drop("State");
-    let mut model = Hyperparameters::new(4)
-        .learning_rate(1.0)
-        .l2_penalty(0.5)
-        .l1_penalty(0.0)
+    let y = df.by("Profit").clone();
+    let mut X = df.idx_drop(4);
+    let dummies = X.get_dummies("State");
+    X = X.concat(dummies).drop("State");
+    let X = &Array::from(&X.values());
+    let y = &Array::from(y.values().to_vec());
+    let mut tree_params = decision_tree::Hyperparameters::new(X.cols());
+    tree_params.min_samples_split(10)
+        .max_features(4);
+
+    let mut model = Hyperparameters::new(6)
         .one_vs_rest();
 
-    model.fit(&X, &y).unwrap();
+    model.fit(X, y).unwrap();
 
-    let prediction = model.predict(&X).unwrap();
-    println!("{:?}", prediction);
+// Optionally serialize and deserialize the model
+
+// let encoded = bincode::serialize(&model).unwrap();
+// let decoded: OneVsRestWrapper<RandomForest> = bincode::deserialize(&encoded).unwrap();
+
+    let prediction = model.predict(X).unwrap();
+    let a = 4;
 }
